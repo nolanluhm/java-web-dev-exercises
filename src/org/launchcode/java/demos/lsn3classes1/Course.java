1fdb75712270b039c2aa371bb272219e0b871420
Nolan <nolan@ADM-122-201617.local>
2020-05-20 14:08:51 -0500

    Completed Chapter 4 exercises.

--- src/org/launchcode/java/demos/lsn3classes1/Course.java
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -1,4 +1,20 @@
 package org.launchcode.java.demos.lsn3classes1;
 
 public class Course {
+
+    private double creditHours;
+    private String courseTitle;
+    private ArrayList<Student> classRoster;
+
+    public Course (creditHours, courseTitle, classRoster) {
+        this.creditHours = creditHours;
+        this.courseTitle = courseTitle;
+        this.classRoster = classRoster;
+    }
+
+    public Course (creditHours, courseTitle) {
+        this.creditHours = creditHours;
+        this.courseTitle = courseTitle;
+    }
+
 }
