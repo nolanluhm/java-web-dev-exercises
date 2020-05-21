8df0156562fd4579bc528bafbc7318cdf63e9ea8
Nolan <nolan@ADM-122-201617.local>
2020-05-21 10:05:58 -0500

    Bug fixing on lsn3classes1

--- src/org/launchcode/java/demos/lsn3classes1/Course.java
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -1,18 +1,20 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+import java.util.ArrayList;
+
 public class Course {
 
     private double creditHours;
     private String courseTitle;
     private ArrayList<Student> classRoster;
 
-    public Course (creditHours, courseTitle, classRoster) {
+    public Course (double creditHours, String courseTitle, ArrayList<Student> classRoster) {
         this.creditHours = creditHours;
         this.courseTitle = courseTitle;
         this.classRoster = classRoster;
     }
 
-    public Course (creditHours, courseTitle) {
+    public Course (double creditHours, String courseTitle) {
         this.creditHours = creditHours;
         this.courseTitle = courseTitle;
     }
